f23910906a411816a6a2676e72acfa10f80f0269
antoniusarie <antonius.arie@gmail.com>
2019-11-09 11:30:29 +0700

    edit login page & add css dropdown menu

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,16 +4,6 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<html>
-<head>
-	<title>STIKOM</title>
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/fontawesome.min.css">
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js"></script>
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/fontawesome.min.js"></script>
-</head>
-<body style="background-color: #e5e7e9;">
-
 <center>
 	<p style="margin: 25vh 25vw;"><i class="fa fa-coffee"></i> 
 		<span><b style="color: brown">KOPI</b> - <i>Karena Otak Perlu Inspirasi</i></span>
